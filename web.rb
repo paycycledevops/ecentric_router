aef7b9c629b707e74a9db68f47ba059c0ccd13bc
Jono Booth <jonobth@gmail.com>
2018-07-17 20:23:47 +0200

    fix: url update

--- web.rb
+++ web.rb
@@ -3,6 +3,6 @@ require 'httparty'
 
 post '/' do
   content_type :html
-  response = HTTParty.post('https://64d9108d.ngrok.io/ecentric/credit_card/tds_lookup', body: params)
+  response = HTTParty.post('https://64d9108d.ngrok.io/ecentric/credit_card/capture_cvv', body: params)
   return response.body
 end
