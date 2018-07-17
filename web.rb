f44931221442ea51b23c2dfbf1c47effdc37264a
Jono Booth <jonobth@gmail.com>
2018-07-17 15:05:03 +0200

    fix: http response

--- web.rb
+++ web.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'httparty'
 
 post '/' do
+  content_type :html
   response = HTTParty.post('https://64d9108d.ngrok.io/ecentric/authorise', body: params)
-  puts response.body
+  return response.body
 end
