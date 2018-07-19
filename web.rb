975b2fe47b5b5920c346fbbf3b5fad1d1b85b650
Jono Booth <jonobth@gmail.com>
2018-07-19 16:17:01 +0200

    fix: staging

--- web.rb
+++ web.rb
@@ -3,6 +3,6 @@ require 'httparty'
 
 post '/' do
   content_type :html
-  response = HTTParty.post('https://64d9108d.ngrok.io/ecentric/credit_card/capture_cvv', body: params)
+  response = HTTParty.post('https://paycyclestaging.herokuapp.com/ecentric/credit_card/capture_cvv', body: params)
   return response.body
 end
