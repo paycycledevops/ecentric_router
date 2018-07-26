282d147c6cbd524bec0a67b7d46b80bb03d6db76
Jono Booth <jonobth@gmail.com>
2018-07-26 13:30:32 +0200

    fix: update url

--- web.rb
+++ web.rb
@@ -3,6 +3,6 @@ require 'httparty'
 
 post '/' do
   content_type :html
-  response = HTTParty.post("#{ENV['domain']}/ecentric/credit_card/capture_cvv", body: params)
+  response = HTTParty.post("#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv", body: params)
   return response.body
 end
