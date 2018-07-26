d5bcf2f28d2e32bad522e4e952e046f275767f53
Jono Booth <jonobth@gmail.com>
2018-07-26 13:29:37 +0200

    fix: update url

--- web.rb
+++ web.rb
@@ -3,6 +3,6 @@ require 'httparty'
 
 post '/' do
   content_type :html
-  response = HTTParty.post('https://64d9108d.ngrok.io/ecentric/credit_card/capture_cvv', body: params)
+  response = HTTParty.post("#{ENV['domain']}/ecentric/credit_card/capture_cvv", body: params)
   return response.body
 end
