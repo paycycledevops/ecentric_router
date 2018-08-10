052c4e060446b2eb961453d41ecb264136b22fa0
jono-booth <jonobth@gmail.com>
2018-08-10 09:25:10 +0200

    fix: redirect instead of post response

--- web.rb
+++ web.rb
@@ -2,7 +2,5 @@ require 'sinatra'
 require 'httparty'
 
 post '/' do
-  content_type :html
-  response = HTTParty.post("#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv", body: params)
-  return response.body
+  redirect "#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv?Token=#{params[:Token]}&UserID=#{params[:UserID]}}"
 end
