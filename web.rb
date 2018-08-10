fedb28687a810a5af3f245aedfa47e511f65b119
jono-booth <jonobth@gmail.com>
2018-08-10 09:33:59 +0200

    fix: url

--- web.rb
+++ web.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 require 'httparty'
 
 post '/' do
-  redirect "#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv?Token=#{params[:Token]}&UserID=#{params[:UserID]}}"
+  redirect "#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv?Token=#{params[:Token]}&UserID=#{params[:UserID]}"
 end
