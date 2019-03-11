48c5e9faa49ce2465c9e3348ffbbe527e238b259
Jono Booth <jonobth@gmail.com>
2019-03-11 13:31:56 +0200

    add:

--- web.rb
+++ web.rb
@@ -4,6 +4,7 @@ require 'httparty'
 post '/' do
   content_type :html
   #response = HTTParty.post("#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv", body: params)
-  redirect "#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv?Token=#{params[:Token]}&UserID=#{params[:UserID]}"
+  query_string = params.map{ |p| p.join('=')}.join('&')
+  redirect "#{ENV['DOMAIN']}/ecentric/credit_card/capture_cvv?Token=#{query_string}"
   return response.body
 end
